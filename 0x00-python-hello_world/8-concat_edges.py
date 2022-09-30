f60ed5f73ccadf0263b809dcc8e1d4c0736c766c
“david <benadejidavid@gmail.com>
2022-09-30 14:36:55 +0100

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:66]} {str[107:111]} {str[:6]}")
+print("object-oriented programming with Python")
 # print(str)
