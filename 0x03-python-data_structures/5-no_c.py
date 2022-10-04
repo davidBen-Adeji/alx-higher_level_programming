54a40f5232bfe7162d16c2c5c8a84ec64dd4aa60
“david <benadejidavid@gmail.com>
2022-10-04 14:03:44 +0100

    5-no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,9 +5,6 @@ def no_c(my_string):
     new_string = ""
     length = len(my_string)
 
-    if length <= 0:
-        return
-
     for i in range(length):
         if my_string[i] == "c" or my_string[i] == "C":
             continue
