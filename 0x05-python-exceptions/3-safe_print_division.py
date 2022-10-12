36890a1cefd11ff928c64e6b9f57315c3f477c77
david ben-adeji <benadejidavid@gmail.com>
2022-10-12 15:02:20 +0100

    3-safe_print

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        result = None
+        return None
+    finally:
+        print("Inside result: {}".format(result))
