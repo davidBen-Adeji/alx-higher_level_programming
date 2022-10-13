c27befaabb38bf481e3234022fc8ffbcf13ddb07
david ben-adeji <benadejidavid@gmail.com>
2022-10-13 12:16:24 +0100

    1-search_replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    length = len(my_list)
+    new_list = my_list.copy()
+    for i in range(length):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+    return new_list
